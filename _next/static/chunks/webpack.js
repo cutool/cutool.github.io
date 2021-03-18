1ef3ea17af48d4a3f9f3af440da8ecb34a4cd290
xuwei <xuwei@dayuwuxian.com>
2021-03-18 23:34:00 +0800

    feature: add 生活

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "01386fb0eae02a68831e";
+/******/ 	var hotCurrentHash = "74d5f5939f41e56bb767";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
