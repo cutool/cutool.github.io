18f7e6dfd080b21baaa8bbea9f718e971992d18a
xuwei <xuwei@dayuwuxian.com>
2021-03-22 23:18:19 +0800

    feature: update theme

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "424c3a34f6436dbf6582";
+/******/ 	var hotCurrentHash = "bc447a076b4a647cff9c";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- jsonedit.js
+++ jsonedit.js
@@ -1,72 +1,89 @@
-window.onload = () => {
+
+function addLoadEvent(func){ 
+    var oldonload=window.onload; 
+    console.log(oldonload);
+    if(typeof window.onload!='function'){ 
+        window.onload=func; 
+    }else{ 
+        window.onload=function(){ 
+            oldonload(); 
+            func(); 
+        } 
+    } 
+    console.log(window.onload);
+} 
+
+addLoadEvent(
+    () => {
     
- const container = document.getElementById("jsoneditor")
-
- const options = {
-    mode: 'tree',
-    modes: ['tree','preview','code']
-    // ajv: Ajv({ 
-    //     allErrors: true, 
-    //     verbose: true,
-    //     jsonPointers: false,
-    //     $data: true
-    //   }),
-    //  onValidate: function (json) {
-    //     var errors = [];
-    
-    //     if (json && json.customer && !json.customer.address) {
-    //       errors.push({
-    //         path: ['customer'],
-    //         message: 'Required property "address" missing.'
-    //       });
-    //     }
-    
-    //     return errors;
-    //   }
- }
- const editor = new JSONEditor(container, options)
- const srcOptions = {
-    mode: "code",
-    mainMenuBar: false,
-    onChangeText: function(json) {
-        // editor.set(editorsrc.get())
-       editor.set(JSON.parse(json));
-    //    editor.set(json)
-        editor.expandAll();
-    },  ajv: Ajv({ 
-        allErrors: true, 
-        verbose: true,
-        jsonPointers: false,
-        $data: true
-      })
-}
- const editorsrc = new JSONEditor(document.getElementById("json-source"), srcOptions)
- // set json
- const initialJson = {
-     "Array": [1, 2, 3],
-     "Boolean": true,
-     "Null": null,
-     "Number": 123,
-     "Object": {"a": "b", "c": "d"},
-     "String": "Hello World"
- }
-//  editor.set(initialJson)
-//  editor.expandAll();
-
- // get json
- const updatedJson = editor.get()
- const jsonSrc = document.getElementById("json-src")
-//  jsonSrc.addEventListener('input', (e) => {
-//     editor.set(jsonSrc.value);editor.expandAll();
-
-
-//  });
-
-        
-//  jsonSrc.addEventListener('propertychange',function () {//兼容IE
-//     editor.set(jsonSrc.value);editor.expandAll();
-
-// });
-
-
-}
+        const container = document.getElementById("jsoneditor")
+       
+        const options = {
+           mode: 'tree',
+           modes: ['tree','preview','code']
+           // ajv: Ajv({ 
+           //     allErrors: true, 
+           //     verbose: true,
+           //     jsonPointers: false,
+           //     $data: true
+           //   }),
+           //  onValidate: function (json) {
+           //     var errors = [];
+           
+           //     if (json && json.customer && !json.customer.address) {
+           //       errors.push({
+           //         path: ['customer'],
+           //         message: 'Required property "address" missing.'
+           //       });
+           //     }
+           
+           //     return errors;
+           //   }
+        }
+        const editor = new JSONEditor(container, options)
+        const srcOptions = {
+           mode: "code",
+           mainMenuBar: false,
+           onChangeText: function(json) {
+               // editor.set(editorsrc.get())
+              editor.set(JSON.parse(json));
+           //    editor.set(json)
+               editor.expandAll();
+           },  ajv: Ajv({ 
+               allErrors: true, 
+               verbose: true,
+               jsonPointers: false,
+               $data: true
+             })
+       }
+        const editorsrc = new JSONEditor(document.getElementById("json-source"), srcOptions)
+        // set json
+        const initialJson = {
+            "Array": [1, 2, 3],
+            "Boolean": true,
+            "Null": null,
+            "Number": 123,
+            "Object": {"a": "b", "c": "d"},
+            "String": "Hello World"
+        }
+       //  editor.set(initialJson)
+       //  editor.expandAll();
+       
+        // get json
+        const updatedJson = editor.get()
+        const jsonSrc = document.getElementById("json-src")
+       //  jsonSrc.addEventListener('input', (e) => {
+       //     editor.set(jsonSrc.value);editor.expandAll();
+       
+       
+       //  });
+       
+               
+       //  jsonSrc.addEventListener('propertychange',function () {//兼容IE
+       //     editor.set(jsonSrc.value);editor.expandAll();
+       
+       // });
+       
+       
+       }
+)
