da56d8cba1d470e5e413899fb51e41afe46c29d6
xuwei <xuwei@dayuwuxian.com>
2021-04-24 01:16:21 +0800

    feature: add sql format

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0863182ba7d287428e59";
+/******/ 	var hotCurrentHash = "31d656fb621b425b2658";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
