00b4a3b05c3276296c9ff6fd2435f2a4d1cee98f
xuwei <xuwei@dayuwuxian.com>
2022-04-26 21:47:43 +0800

    publish

--- jsonedit.js
+++ jsonedit.js
@@ -1,4 +1,3 @@
-
 function addLoadEvent(func){ 
     var oldonload=window.onload; 
     console.log(oldonload);
@@ -13,115 +12,159 @@ function addLoadEvent(func){
     console.log(window.onload);
 } 
 
-addLoadEvent(
-    () => {
-    
-        const container = document.getElementById("jsoneditor")
-      
-        const jsonEditorError = document.getElementById("jsoneditorerror")
-        
-        const options = {
-           mode: 'code',
-           modes: ['tree','preview','code','form'],
-           // ajv: Ajv({ 
-           //     allErrors: true, 
-           //     verbose: true,
-           //     jsonPointers: false,
-           //     $data: true
-           //   }),
-           //  onValidate: function (json) {
-           //     var errors = [];
-           
-           //     if (json && json.customer && !json.customer.address) {
-           //       errors.push({
-           //         path: ['customer'],
-           //         message: 'Required property "address" missing.'
-           //       });
-           //     }
-           
-           //     return errors;
-           //   }
-        }
-        const editor = new JSONEditor(container, options)
-        const srcOptions = {
-           mode: "code",
-           mainMenuBar: false,
-           popupAnchor: document.getElementById("#loading"),
-           onChangeText: function(json) {
-               // editor.set(editorsrc.get())
-               try{
+function handleTextChanged(){
 
-                if (json==""){
-               jsonEditorError.setAttribute("class","dNone");
-               container.setAttribute("class","dBlock");
-                }
-              editor.set(JSON.parse(json));
-               jsonEditorError.setAttribute("class","dNone");
-               container.setAttribute("class","dBlock");
-                editor.expandAll();
-               }catch(e){
-                   console.log(e)
-                //    editor.set({});
-               }
-           },  ajv: Ajv({ 
-               allErrors: true, 
-               verbose: true,
-               jsonPointers: false,
-               $data: true,
-               
-             }),
-             onValidationError: function (errors) {
-                 try{
-                     jsonEditorError.innerHTML="";
-                     errorHtml = "";
-                     for(var i=0;i<errors.length;i++) {
-                        errorHtml = errorHtml + "<div>"
-                        errorHtml = errorHtml + errors[i].message
-                        errorHtml = errorHtml + "</div><br>"
-                     }
-                     jsonEditorError.innerHTML = errorHtml;
-                     if (errorHtml!=""){
-                        container.setAttribute("class","dNone");
-                        jsonEditorError.setAttribute("class","dBlock");
-                     }
-                 }catch(e){
-                     console.log(e)
-                 }
-            }
-       }
-        const editorsrc = new JSONEditor(document.getElementById("json-source"), srcOptions)
-        // set json
-        const initialJson = {
-            "Array": [1, 2, 3],
-            "Boolean": true,
-            "Null": null,
-            "Number": 123,
-            "Object": {"a": "b", "c": "d"},
-            "String": "Hello World"
+    var options = {
+        collapsed: false,
+        withQuotes:true
+    }
+    try {
+        if (jQuery == undefined){
+            console.log("jQuery is undefined")
+            return;
         }
-       //  editor.set(initialJson)
-       //  editor.expandAll();
+        var result = jsonlint.parse(document.getElementById("json-source-text").value);
+        if (result) {
+        //   document.getElementById("json-render").innerHTML = "JSON is valid!";
+        //   document.getElementById("json-render").className = "pass";
+        //   if (document.getElementById("json-render").checked) {
+        //     document.getElementById("json-render").value = JSON.stringify(result, null, "  ");
+        //   }
+        var jsonStr =eval('('+$('#json-source-text').val()+')')
+        // alert(jsonStr)
+         $("#json-render").jsonViewer(jsonStr,options)
+        //   document.getElementById("json-render").value = JSON.stringify(document.getElementById("json-source-text").value, null, "  ");
+        document.getElementById("json-render").className = "success";
+    
+        }
+      } catch(e) {
+        document.getElementById("json-render").innerHTML = e;
+        document.getElementById("json-render").className = "fail";
+      }
+}
+
+addLoadEvent(
+    () =>{
+        if (document.getElementById("json-source-text").value!=''){
+            handleTextChanged();
+        }
+        document.getElementById("json-source-text").onkeyup = function () {
+            handleTextChanged();
+        };
+      }
+
+)
+// addLoadEvent(
+//     () => {
+    
+//         const container = document.getElementById("jsoneditor")
+      
+//         const jsonEditorError = document.getElementById("jsoneditorerror")
+        
+//         const options = {
+//            mode: 'code',
+//            modes: ['tree','preview','code','form'],
+//            // ajv: Ajv({ 
+//            //     allErrors: true, 
+//            //     verbose: true,
+//            //     jsonPointers: false,
+//            //     $data: true
+//            //   }),
+//            //  onValidate: function (json) {
+//            //     var errors = [];
+           
+//            //     if (json && json.customer && !json.customer.address) {
+//            //       errors.push({
+//            //         path: ['customer'],
+//            //         message: 'Required property "address" missing.'
+//            //       });
+//            //     }
+           
+//            //     return errors;
+//            //   }
+//         }
+//         const editor = new JSONEditor(container, options)
+//         const srcOptions = {
+//            mode: "code",
+//            mainMenuBar: false,
+//            popupAnchor: document.getElementById("#loading"),
+//            onChangeText: function(json) {
+//                // editor.set(editorsrc.get())
+//                try{
+
+//                 if (json==""){
+//                jsonEditorError.setAttribute("class","dNone");
+//                container.setAttribute("class","dBlock");
+//                 }
+//               editor.set(JSON.parse(json));
+//                jsonEditorError.setAttribute("class","dNone");
+//                container.setAttribute("class","dBlock");
+//                 editor.expandAll();
+//                }catch(e){
+//                    console.log(e)
+//                 //    editor.set({});
+//                }
+//            },  ajv: Ajv({ 
+//                allErrors: true, 
+//                verbose: true,
+//                jsonPointers: false,
+//                $data: true,
+               
+//              }),
+//              onValidationError: function (errors) {
+//                  try{
+//                      jsonEditorError.innerHTML="";
+//                      errorHtml = "";
+//                      for(var i=0;i<errors.length;i++) {
+//                         errorHtml = errorHtml + "<div>"
+//                         errorHtml = errorHtml + errors[i].message
+//                         errorHtml = errorHtml + "</div><br>"
+//                      }
+//                      jsonEditorError.innerHTML = errorHtml;
+//                      if (errorHtml!=""){
+//                         container.setAttribute("class","dNone");
+//                         jsonEditorError.setAttribute("class","dBlock");
+//                      }
+//                  }catch(e){
+//                      console.log(e)
+//                  }
+//             }
+//        }
+//         const editorsrc = new JSONEditor(document.getElementById("json-source"), srcOptions)
+//         // set json
+//         const initialJson = {
+//             "Array": [1, 2, 3],
+//             "Boolean": true,
+//             "Null": null,
+//             "Number": 123,
+//             "Object": {"a": "b", "c": "d"},
+//             "String": "Hello World"
+//         }
+//        //  editor.set(initialJson)
+//        //  editor.expandAll();
        
-        // get json
-        const updatedJson = editor.get()
-        const jsonSrc = document.getElementById("json-src")
-       //  jsonSrc.addEventListener('input', (e) => {
-       //     editor.set(jsonSrc.value);editor.expandAll();
+//         // get json
+//         const updatedJson = editor.get()
+//         const jsonSrc = document.getElementById("json-src")
+//        //  jsonSrc.addEventListener('input', (e) => {
+//        //     editor.set(jsonSrc.value);editor.expandAll();
        
        
-       //  });
+//        //  });
        
                
-       //  jsonSrc.addEventListener('propertychange',function () {//兼容IE
-       //     editor.set(jsonSrc.value);editor.expandAll();
+//        //  jsonSrc.addEventListener('propertychange',function () {//兼容IE
+//        //     editor.set(jsonSrc.value);editor.expandAll();
        
-       // });
+//        // });
        
-       const loading = document.getElementById("loading")
-       loading.setAttribute("style","display:none")
-       const content = document.getElementById("content")
-    //    content.setAttribute("style","display:block")
-        // content.setAttribute("style","display:none")
+//        const loading = document.getElementById("loading")
+//        if (loading!=null){
+//         loading.setAttribute("style","display:none")
+//        }
+//        const content = document.getElementById("content")
+//     //    content.setAttribute("style","display:block")
+//         // content.setAttribute("style","display:none")
  
-       }
-)
+//        }
+// )
